8535dd8195d232e4e6c117438b75bf06583898ce
halfmoon-mind <5338095@gmail.com>
2026-07-03 02:24:03 +0900

    fix(react): Tooltip 스토리 Provider 래핑 + 브리지 base 레이어 (다크 배경 미전환 수정)

--- packages/react/src/components/ui/tooltip.stories.tsx
+++ packages/react/src/components/ui/tooltip.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Button } from '@/components/ui/button';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
+import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from '@/components/ui/tooltip';
 
 const meta = { component: Tooltip } satisfies Meta<typeof Tooltip>;
 export default meta;
@@ -8,11 +8,13 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   render: () => (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <Button variant="outline">호버하세요</Button>
-      </TooltipTrigger>
-      <TooltipContent>halfmoon 토큰으로 테마링된 툴팁</TooltipContent>
-    </Tooltip>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="outline">호버하세요</Button>
+        </TooltipTrigger>
+        <TooltipContent>halfmoon 토큰으로 테마링된 툴팁</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   ),
 };
